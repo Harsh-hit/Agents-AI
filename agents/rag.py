fb780a4b7677dcb15fe4c363fb04337f3c73bd8b
GANDHAVALLA Harshith <harshith.gandhavalla@hexagon.com>
2025-03-21 20:05:20 +0530

    added RAG based approach with sample data

--- agents/rag.py
+++ agents/rag.py
@@ -1,4 +1,11 @@
 from pypdf import PdfReader
+import google.generativeai as genai
+import re
+from chromadb import Documents, EmbeddingFunction, Embeddings, Collection
+import os
+import chromadb
+from dotenv import load_dotenv
+load_dotenv()
 
 def load_pdf(file_path):
     """
@@ -20,6 +27,119 @@ def load_pdf(file_path):
 
     return text
 
-# replace the path with your file path
-pdf_text = load_pdf(file_path=r"./agents/media/Complete_AutoCAD_Commands.pdf")
-print(pdf_text[:1000]) # Display the first 1000 characters of the PDF text
+def split_text(text: str):
+    """
+    Splits a text string into a list of non-empty substrings based on the specified pattern.
+    The "\n \n" pattern will split the document para by para
+    Parameters:
+    - text (str): The input text to be split.
+
+    Returns:
+    - List[str]: A list containing non-empty substrings obtained by splitting the input text.
+
+    """
+    split_text = re.split('\n \n', text)
+    return [i for i in split_text if i != ""]
+
+class GeminiEmbeddingFunction(EmbeddingFunction):
+    """
+    Custom embedding function using the Gemini AI API for document retrieval.
+
+    This class extends the EmbeddingFunction class and implements the __call__ method
+    to generate embeddings for a given set of documents using the Gemini AI API.
+
+    Parameters:
+    - input (Documents): A collection of documents to be embedded.
+
+    Returns:
+    - Embeddings: Embeddings generated for the input documents.
+    """
+    def __call__(self, input: Documents) -> Embeddings:
+        genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+        model = "models/embedding-001"
+        title = "Custom query"
+        return genai.embed_content(model=model,
+                                   content=input,
+                                   task_type="retrieval_document",
+                                   title=title)["embedding"]
+def create_chroma_db(documents:list, path:str, name:str):
+    """
+    Creates a Chroma database using the provided documents, path, and collection name.
+
+    Parameters:
+    - documents: An iterable of documents to be added to the Chroma database.
+    - path (str): The path where the Chroma database will be stored.
+    - name (str): The name of the collection within the Chroma database.
+
+    Returns:
+    - Tuple[chromadb.Collection, str]: A tuple containing the created Chroma Collection and its name.
+    """
+    chroma_client = chromadb.PersistentClient(path=path)
+    db = chroma_client.create_collection(name=name, embedding_function=GeminiEmbeddingFunction())
+
+    for i, d in enumerate(documents):
+        db.add(documents=d, ids=str(i))
+
+    return db, name
+
+def load_chroma_collection(path, name):
+    """
+    Loads an existing Chroma collection from the specified path with the given name.
+
+    Parameters:
+    - path (str): The path where the Chroma database is stored.
+    - name (str): The name of the collection within the Chroma database.
+
+    Returns:
+    - chromadb.Collection: The loaded Chroma Collection.
+    """
+    chroma_client = chromadb.PersistentClient(path=path)
+    db = chroma_client.get_collection(name=name, embedding_function=GeminiEmbeddingFunction())
+
+    return db
+
+
+
+pdf_text = load_pdf(file_path="./media/Complete_AutoCAD_Commands.pdf") #replace with your path
+chunked_text = split_text(text=pdf_text)
+
+# db,name =create_chroma_db(documents=chunked_text, 
+#                           path="./media", #replace with your path
+#                           name="rag_experiment")
+db=load_chroma_collection(path="./media", name="rag_experiment")
+def get_relevant_passage(query, db:Collection, n_results):
+  passage = db.query(query_texts=[query], n_results=n_results)['documents'][0]
+  return passage
+
+#Example usage
+relevant_text = get_relevant_passage(query="Commands to draw a circle",db=db,n_results=3)
+def make_rag_prompt(query, relevant_passage:str):
+    escaped = relevant_passage.replace("'", "").replace('"', "").replace("\n", " ")
+    prompt = ("""You are a AutoCad expert bot that answers questions using text from the reference passage included below. \
+    Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
+    However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
+    strike a friendly and converstional tone. \
+    If the passage is irrelevant to the answer, you may ignore it.
+    QUESTION: '{query}'
+    PASSAGE: '{relevant_passage}'
+
+    ANSWER:
+    """).format(query=query, relevant_passage=escaped)
+    return prompt
+  
+def generate_answers(prompt):
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+    model = genai.GenerativeModel('gemini-2.0-flash')
+    answer = model.generate_content(prompt)
+    return answer.text
+
+def generate_answer(db,query):
+    #retrieve top 3 relevant text chunks
+    relevant_text = get_relevant_passage(query,db,n_results=3)
+    prompt = make_rag_prompt(query, 
+                             relevant_passage="".join(relevant_text)) # joining the relevant chunks to create a single passage
+    answer = generate_answers(prompt)
+
+    return answer
+answer = generate_answer(db,query="what are the commands required to draw a circle of radius 5?")
+print(answer)
